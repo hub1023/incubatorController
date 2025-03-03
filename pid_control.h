4b35c237bab66cbecf141a878a1a57810786c313
hub1023 <neil.bonilla@gmail.com>
2025-03-04 02:36:07 +1300

    Add files via upload

---
+++ pid_control.h
@@ -0,0 +1,22 @@
+/*
+ * pid_control.h
+ *
+ *  Created on: Mar 3, 2025
+ *      Author: PlasmaPC
+ */
+
+#include "AHT10.h"
+#include "setpoints.h"
+#include "uart_config.h"
+
+#ifndef INC_PID_CONTROL_H_
+#define INC_PID_CONTROL_H_
+
+
+
+float pidControl(float error, float feedForward, PIDControl* pidCtrl);
+void adjustHeating(float pidOutput);
+void controlTemperature(void);
+void controlVentilation(void);
+
+#endif /* INC_PID_CONTROL_H_ */
